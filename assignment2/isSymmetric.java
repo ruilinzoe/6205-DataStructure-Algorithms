30213c6fa59d60d0a14fc0b8b5944a1ae03b1d71
Ruilin Zoe <angel2007zoe@gmail.com>
2022-10-13 17:35:06 -0700

    a2

---
+++ assignment2/isSymmetric.java
@@ -0,0 +1,18 @@
+package assignment2;
+
+public class boolean isSymmetric(TreeNode root) {
+        TreeNode left=
+        if(sameNode(root.left, root.right)){
+        return sameNode()
+        }
+        }
+
+private boolean sameNode(TreeNode first, TreeNode second){
+        if(first.val=second.val){
+        if(first.left!=null && second.right!=null){
+
+        }
+        return sameNode(first.);
+        }
+        return false;
+        }
