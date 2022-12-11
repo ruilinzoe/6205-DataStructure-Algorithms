c53a27faa3ad55e3d9dc377cc99ca2510312be9b
ruilinzoe <angel2007zoe@gmail.com>
2022-12-11 11:13:11 -0800

    final

--- finalexam/parenthesis.java
+++ finalexam/parenthesis.java
@@ -1,26 +1,33 @@
 package finalexam;
 
 public class parenthesis {
-    private StringBuilder removeInvalid(CharSequence string, char open, char close){
+    private static StringBuilder removeInvalid(CharSequence string, char open, char close){
         StringBuilder sb=new StringBuilder();
-        int balance=0;
+        int count=0;
         for(int i=0; i<string.length(); i++){
             char c=string.charAt(i);
             if(c==open){
-                balance++;
+                count++;
             }if(c==close){
-                if(balance==0)continue;
-                balance--;
+                if(count==0)continue;
+                count--;
             }
             sb.append(c);
         }
         return sb;
     }
 
-    public String minRemoveToMakeValid(String s) {
+    public static String parenthesis(String s) {
         StringBuilder result=removeInvalid(s,'(',')');
         result=removeInvalid(result.reverse(),')','(');
         return result.reverse().toString();
     }
+
+    public static void main(String[] args) {
+        String s=")((apple)))";
+        System.out.printf(parenthesis(s));
+    }
+    //Time O(N); n is the length of String s
+    //Space O(N); have used string builder
 
 }
